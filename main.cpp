958ac36e41125c103eabb0683d540e9e11861aa7
chris <t@cmgwallerstein.net>
2020-03-18 20:27:24 -0400

    Take input from user

--- main.cpp
+++ main.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
-#include <string>
-#include <sstream>
-#include <vector>
-#include <cassert>
 
-using namespace std;
-
-void getTokens(const string& intermediate, vector<string>& tokens);
+#include "state_machine.h"
+#include "node_writer.h"
 
 int main() {
-    string input  = "(first (list 1 (+ 2 3) 9))";
-    stringstream str_stream(input);
-    string intermediate;
-    vector<string> tokens;
-    while(getline(str_stream, intermediate, ' ')) {
-        getTokens(intermediate, tokens);
+    std::cout << "Type your LISP to be interpreted:\n";
+    string response;
+    getline(std::cin, response);
+    std::cout << response << std::endl;
+    StateContext state_context;
+    std::string accumulator;
+    auto root_node = std::make_shared<Node> ();
+
+    auto current_node = state_context.handleCharacter
+            (response.at (0), accumulator, root_node);
+    for (int i = 1; i < response.size (); ++i) {
+        current_node = state_context.handleCharacter
+                (response.at (i), accumulator, current_node);
     }
-    for (auto &token : tokens) {
-        cout << token << " ";
-    }
-    cout << endl;
+
+    std::cout << "Done parsing\n";
+
+    NodeWriter<std::ostream> writer (std::cout);
+    writer.writeNode (root_node);
+    std::cout << std::endl;
+
+    return 0;
 }
 
